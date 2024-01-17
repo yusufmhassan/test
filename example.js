decd0c174f837b5f403ff2e1cc959533d5946ce4
yusufmhassan <you@example.com>
2024-01-17 17:02:45 +0100

    hello

---
+++ example.js
@@ -0,0 +1,20 @@
+function quadratic(a, b, c) {
+    const topEquation = b**2 -4*a*c;
+    const x1 = (-b + math.sqrt.(topEquation)) / (2 * a);
+    const x2 = (-b - math.sqrt.(topEquation)) / (2 * a);
+    console.log(x1, x2);
+}
+quadratic(3, 4 , 6)
+
+function equation(a, b, d) {
+    const upEquation =  b**2 -4*a*d;
+    const x3 = (-b + math.sqrt(upEquation)) / (2*a);
+    const x4 = (-b + math.sqrt(upEquation)) / (2*a);
+    console.log(x3,x4);
+
+
+
+
+}
+
+equation( 1, 2, 3);
